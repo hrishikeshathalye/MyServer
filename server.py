f5ca9114ad92adfb44bb7d1df3e2c57349fe7816
Hrishikesh Athalye <hathalye7@gmail.com>
2020-10-05 00:33:44 +0530

    corrected response added crlf after header

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ while True:
 	client_connection, client_address = listen_socket.accept()
 	request_data = client_connection.recv(1024)
 	print(request_data.decode('utf-8'))
-	http_response = b"HTTP/1.1 200 OK\nHello, World!\n"
+	http_response = b"""\
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
 	client_connection.sendall(http_response)
 	client_connection.close()
