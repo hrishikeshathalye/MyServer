582f490c5a50204144a98f007e3ee42da39d7214
Hrishikesh Athalye <hathalye7@gmail.com>
2020-10-05 09:33:14 +0530

    encapsulated socket functionality

--- server.py
+++ server.py
@@ -1,22 +1,47 @@
 import socket
 
-HOST, PORT = '', 90
+#class to encapsulate most socket functions
+class tcpSocket:
+	def __init__(self, host, port):
+		"""
+		creates tcp socket, binds to host and port, makes it listen
+		"""
+		self.socketVar = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#SO_REUSEADDR allows reuse of sockets set in TIME_WAIT state
+		#it also does not block all ports hence to be used if using HOST as ''
+		self.socketVar.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socketVar.bind((host, port))
+		self.socketVar.listen(5)
+		print(f'Serving HTTP on port {port} ...')
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#SO_REUSEADDR allows reuse of sockets set in TIME_WAIT state
-#it also does not block all ports hence to be used if using HOST as ''
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print(f'Serving HTTP on port {PORT} ...')
-while True:
-	client_connection, client_address = listen_socket.accept()
-	request_data = client_connection.recv(1024)
-	print(request_data.decode('utf-8'))
-	http_response = b"""\
+	def accept(self):
+		self.clientConnection, self.clientAddress = self.socketVar.accept()
+
+	def receive(self, decodingScheme):
+		"""
+		receive and decode according to passed scheme
+		"""
+		return self.clientConnection.recv(1024).decode(decodingScheme)
+
+	def send(self, response, encodingScheme):
+		"""
+		encodes and sends
+		"""
+		self.clientConnection.sendall(response.encode(encodingScheme))
+	
+	def close(self):
+		self.clientConnection.close()
+
+if __name__ == "__main__":
+	tcpSocket = tcpSocket('', 90)
+	while True:
+		tcpSocket.accept()
+		request = tcpSocket.receive('utf-8')
+		print(request)
+		response = """
 HTTP/1.1 200 OK
 
 Hello, World!
 """
-	client_connection.sendall(http_response)
-	client_connection.close()
+		tcpSocket.send(response, 'utf-8')
+		tcpSocket.close()
